35397980b1ae1904a7c2b7e2513a10bf1fbae207
Camp <c4mpunvrst@gmail.com>
2019-03-07 13:22:32 +0600

    add database

---
+++ app/src/main/java/com/andrey/wooppaymessenger/database/MessageViewModel.java
@@ -0,0 +1,27 @@
+package com.andrey.wooppaymessenger.database;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.andrey.wooppaymessenger.database.models.Message;
+
+import java.util.List;
+
+public class MessageViewModel extends AndroidViewModel {
+    private MessageRepository mRepository;
+
+    private LiveData<List<Message>> mAllMessages;
+
+    public MessageViewModel (Application application) {
+        super(application);
+        mRepository = new MessageRepository(application);
+        mAllMessages = mRepository.getAllMessages();
+    }
+
+    public LiveData<List<Message>> getAllMessages(){
+        return mAllMessages;
+    }
+
+    public void insert(Message message) { mRepository.insert(message); }
+}
